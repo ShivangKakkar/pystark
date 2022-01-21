3875704fac6955a14aabf5d63afa69dec6a60a3f
Stark Bots <88478059+StarkBotsIndustries@users.noreply.github.com>
2022-01-22 00:45:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ packages = find_packages()
 setup(
     name='PyStark',
     packages=packages,
-    version='0.1.32',  # 0.2.9
+    version='0.2.9',
     license='GPLv3+',
     description='An incomplete add-on extension to Pyrogram for personal use.',
     long_description=long_description,
